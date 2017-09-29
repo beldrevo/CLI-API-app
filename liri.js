d429b492c8b8b69e4f35a65c197e999c8e79f169
beldrevo <beldrevo.by@gmail.com>
2017-09-28 20:11:51 -0700

    command update

--- liri.js
+++ liri.js
@@ -4,17 +4,17 @@ var Twitter = require("twitter");
 
 var Spotify = require("node-spotify-api");
 
-var cons_key = ref.consumer_key;
-var cons_sec = ref.consumer_secret;
-var aToken_key = ref.access_token_key;
-var aToken_sec = ref.access_token_secret;
+var request = require('request');
 
 var command = process.argv[2];
 var data = process.argv[3];
 
-console.log(data);
-
-var client = new Twitter(ref);
+var client = new Twitter({
+	consumer_key: ref.twitterKeys.consumer_key,
+	consumer_secret: ref.twitterKeys.consumer_secret,
+	access_token_key: ref.twitterKeys.access_token_key,
+	access_token_secret: ref.twitterKeys.access_token_secret
+})
  
 var spotify = new Spotify({
 	id: "45198b7ab09e4a5c9e89f4bf93962c5e",
@@ -22,58 +22,121 @@ var spotify = new Spotify({
 })
 
 function lastTweets() {
-	if (command == "my-tweets") {
-		client.get('favorites/list', function(error, tweets, response) {
-		  if (!error) {
-		    // console.log(JSON.stringify(tweets));
-		    console.log(tweets);
-
-		  } 
+	if (command === "my-tweets") {
+		client.get('favorites/list', function(error, tweets) {
+		    console.log(tweets[0].text);
+		    // console.log(Object.keys(tweets[0].text));
+		    console.log(Object.keys(tweets[0].text));
 		})
 	}
+}
+
+function printMultiline(string) {
+	var lines = string.replace(/(^|\n)\s*/g, '\n').split('\n');
+
+	if (!lines[0].trim()) lines.shift();
+	if (lines.length && !lines[lines.length - 1].trim()) lines.pop();
+
+	console.log(lines.join('\n'));
 }
 
 lastTweets();
 
 function getSong() {
-	if (command === "spotify-this-song") {
+	if (command === "spotify-this-song" && data) {
 		
 		spotify.search({ type: "track", query: data, limit: 1}, function(err, response) {
 			if (err) {
-						return console.log("Error occurred: " + err);
-						// spotify.search({ type: "track", query: "The Sign", limit: 1}, function(err, response) {
-						// 	if (err) {
-						// 				return console.log("Error occured: " + err);
-						// 	}
-						// 		else {
-						// 				console.log(response.tracks.items.artists);
-						// 			 	// console.log(response.tracks.items.name);
-						// 			 	// console.log(response.tracks.items.href);
-						// 			 	// console.log(response.tracks.items.album.name);
-						// 		}
-						// })
-			} 
+				console.log("Error is happened: " + error.statusCode);			
+			}
 			 else {
-			 	console.log("Artist: " + response.tracks.items[0].artists[0].name);
-			 	console.log("=====================================");
-			 	console.log("The song's name: " + response.tracks.items[0].name);
-			 	console.log("=====================================");
-			 	console.log("Preview spotify link: " + response.tracks.items[0].href);
-			 	console.log("=====================================");
-			 	console.log("The album name: " + response.tracks.items[0].album.name);
-			 	console.log("=====================================");
-			 	
-
-			 }
+			 	printMultiline(`
+			 		=====================================
+					Artist: ${response.tracks.items[0].artists[0].name}
+					=====================================
+					The song's name: ${response.tracks.items[0].name}
+					=====================================
+					Preview spotify link: ${response.tracks.items[0].href}
+					=====================================
+					The album name: ${response.tracks.items[0].album.name}
+					=====================================
+				`);
+			}
+		})
+	} 
+	else if (command === "spotify-this-song" && !data) {
+		var data = "The-Sign";
+		spotify.search({ type: "track", query: data, limit: 1}, function(err, response) {
+			if (err) {
+				console.log("Error is happened: " + error.statusCode);			
+			}
+			else {
+				printMultiline(`
+			 		=====================================
+					Artist: ${response.tracks.items[0].artists[0].name}
+					=====================================
+					The song's name: ${response.tracks.items[0].name}
+					=====================================
+					Preview spotify link: ${response.tracks.items[0].href}
+					=====================================
+					The album name: ${response.tracks.items[0].album.name}
+					=====================================
+				`);
+			}
 		})
 	}
-
 }
 
 getSong();
 
-// function movies() {
+function movies() {
+	if (command === "movie-this" && data) {
 
-// }
+		request("https://omdbapi.com/?t=" + data + "&apikey=40e9cece", function (error, response) {
+		  var movie_info = JSON.parse(response.body);
+		  console.log("=====================")
+		  console.log("Title: " + movie_info.Title);
+		  console.log("=====================")
+		  console.log("Year: " + movie_info.Year);
+		  console.log("=====================")
+		  console.log("Rating: " + movie_info.imdbRating);
+		  console.log("=====================")
+		  console.log("Rotten Tomatoes Rating: " + movie_info.Ratings[1].value);
+		  console.log("=====================")
+		  console.log("Country: " + movie_info.Country);
+		  console.log("=====================")
+		  console.log("Language: " + movie_info.Language);
+		  console.log("=====================")
+		  console.log("Plot: " + movie_info.Plot);
+		  console.log("=====================")
+		  console.log("Actors: " + movie_info.Actors);
+		  console.log("=====================")
+		})
+	} 
 
-// movies();
+	else if (command === "movie-this" && !data) {
+		
+		request("https://omdbapi.com/?t=Mr+Nobody&apikey=40e9cece", function (error, response) {
+		  var movie_info = JSON.parse(response.body);
+		  console.log("=====================")
+		  console.log("Title: " + movie_info.Title);
+		  console.log("=====================")
+		  console.log("Year: " + movie_info.Year);
+		  console.log("=====================")
+		  console.log("Rating: " + movie_info.imdbRating);
+		  console.log("=====================")
+		  console.log("Rotten Tomatoes Rating: " + movie_info.Ratings[1].value);
+		  console.log("=====================")
+		  console.log("Country: " + movie_info.Country);
+		  console.log("=====================")
+		  console.log("Language: " + movie_info.Language);
+		  console.log("=====================")
+		  console.log("Plot: " + movie_info.Plot);
+		  console.log("=====================")
+		  console.log("Actors: " + movie_info.Actors);
+		  console.log("=====================")
+		})
+	}
+}
+
+movies();
